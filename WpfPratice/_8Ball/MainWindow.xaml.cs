323c74b25eae93c5cf3b3eda82465205f5066e03
Gesendex <gireevtu@gmail.com>
2021-09-25 14:45:30 +0300

    Задание 5 - Создание адаптивного TextBox

---
+++ WpfPratice/_8Ball/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _8Ball
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtQuestion_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+        private string GetRandomAnswer()
+        {
+            Random rnd = new Random();
+            int a = rnd.Next(1, 4);
+            switch (a)
+            {
+                case 0:
+                    return "Так и есть";
+                case 1:
+                    return "Сомневаюсь";
+                case 2:
+                    return "Никак нет";
+            }
+            return "нет ответа";
+               
+        }
+        private void Answer_Click(object sender, RoutedEventArgs e)
+        {
+            this.Cursor = Cursors.Wait;
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            txtAnswer.Text = GetRandomAnswer();
+            // Восстанавливаем прежнее состояние курсора.
+            this.Cursor = null;
+
+        }
+    }
+}
