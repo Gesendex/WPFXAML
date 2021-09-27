0a17fe26847526643eb71706daa58a502feebd4d
Gesendex <gireevtu@gmail.com>
2021-09-27 11:11:02 +0300

    Вариант 3 и Вариант 4

---
+++ WPF_Containers/WPF_Var4/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Var4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+       
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            lstEditingMode.Items.Add(InkCanvasEditingMode.EraseByPoint);
+            lstEditingMode.Items.Add(InkCanvasEditingMode.EraseByStroke);
+            lstEditingMode.Items.Add(InkCanvasEditingMode.Ink);
+        }
+        private void lstEditingMode_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
