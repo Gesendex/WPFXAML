0e231c37ad34b8005df284fb493e4f4c1637151c
Gesendex <gireevtu@gmail.com>
2021-09-28 21:57:55 +0300

    Дополнительные задания

---
+++ WPF_Controls/Task1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dpBirth.DisplayDateStart = DateTime.Now.AddYears(-60);
+            dpBirth.DisplayDateEnd = DateTime.Now.AddYears(-18);
+        }
+
+        private void dpBirth_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dpBirth.SelectedDate < DateTime.Now.AddYears(-60) ||
+                dpBirth.SelectedDate > DateTime.Now.AddYears(-18))
+            {
+                MessageBox.Show("Введена некорректная дата", "Ошибка",MessageBoxButton.OK,MessageBoxImage.Error);
+                dpBirth.SelectedDate = DateTime.Now.AddYears(-18);
+            }
+            
+        }
+
+    }
+}
---
+++ WPF_Controls/Task2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            dpStart.SelectedDate = DateTime.Today;
+            dpDuration.Text = 0.ToString();
+        }
+
+        private void dpEnd_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dpEnd.SelectedDate.HasValue &&
+                (dpEnd.SelectedDate.Value > dpStart.SelectedDate.Value.AddDays(int.Parse(dpDuration.Text)) || 
+                dpEnd.SelectedDate.Value < dpStart.SelectedDate.Value))
+            {
+                MessageBox.Show("Введена некорректная дата сдачи или срок проекта", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                dpEnd.SelectedDate = null;
+            }
+
+        }
+    }
+}
