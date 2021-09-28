31920f3f248b1dc062e640fa75a0cc44fb4cbd91
Gesendex <gireevtu@gmail.com>
2021-09-28 09:31:54 +0300

    Задание 3. CheckBoxList

--- WPF_Controls/WPF_Controls/MainWindow.xaml.cs
+++ WPF_Controls/WPF_Controls/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPF_Label
+namespace Label
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
