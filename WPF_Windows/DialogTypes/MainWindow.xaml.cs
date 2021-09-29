d2724402814c7168d9034f5ee37d636b3d4e729c
Gesendex <gireevtu@gmail.com>
2021-09-29 11:04:28 +0300

    1 - DialogTypes

---
+++ WPF_Windows/DialogTypes/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DialogTypes
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            NewWindow window = new NewWindow("Модальное");
+            window.ShowDialog();
+        }
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            NewWindow window = new NewWindow("Немодальное");
+            window.Show();
+        }
+
+    }
+}
