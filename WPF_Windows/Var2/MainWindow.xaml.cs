cccb7c8973937c4775668b0ccfd23d025fd55fd6
Gesendex <gireevtu@gmail.com>
2021-09-29 22:21:23 +0300

    Var2

---
+++ WPF_Windows/Var2/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Var2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            if ((bool)fileDialog.ShowDialog())
+            {
+                using (StreamReader fileStream = new StreamReader(fileDialog.FileName))
+                {
+                    rtbText.AppendText(fileStream.ReadToEnd());
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if ((bool)saveFileDialog.ShowDialog())
+            {
+                using (StreamWriter fileStream = new StreamWriter(saveFileDialog.FileName))
+                {
+                    TextRange doc = new TextRange(rtbText.Document.ContentStart, rtbText.Document.ContentEnd);
+                    fileStream.Write(doc.Text);
+                }
+            }
+        }
+    }
+}
